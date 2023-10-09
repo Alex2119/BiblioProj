9cb80be4d28aeef5e4926946e91f7e66d2f4ef38
Alexandre PEROT <alexandre.perot@web-atrio.com>
2023-10-09 10:37:23 +0200

    Modification du Program.cs pour améliorer la connexion.

--- BiblioProj/Program.cs
+++ BiblioProj/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using BiblioProj.Data;
 using System;
+using System.Configuration;
+using Microsoft.Extensions.Options;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<BiblioProjContext>(options =>
@@ -13,9 +15,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-string connectionString = builder.Configuration.GetConnectionString("BiblioProjContext");
+
+
+
 builder.Services.AddDbContext<BiblioProjContext>(options =>
-    options.UseSqlServer(connectionString));
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("BiblioProjContext"));
+    options.UseLoggerFactory(LoggerFactory.Create(builder => builder.AddConsole()));
+    options.EnableSensitiveDataLogging(); // Pour afficher les données sensibles dans les requêtes
+});
+
 
 var app = builder.Build();
 
